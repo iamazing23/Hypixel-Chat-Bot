8e5abbf5bb954a2ff8504fb7d0051f86c5ed3769
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-05 20:10:43 -0700

    aaaa
    aaa

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const { Client, Intents } = require('discord.js-light');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 'use strict'
 process.title = 'Hypixel Discord Chat Bridge'
 
 const app = require('./src/Application')
-const DiscordManager = require('./DiscordManager'); // Replace with the correct path
-const discordManager = new DiscordManager(app, client);
 
 app
   .register()
