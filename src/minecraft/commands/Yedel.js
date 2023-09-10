8816a22b9bc8d7f12f38bff6e1c707b8ed9608c2
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-10 16:20:31 -0700

    multiple response

--- src/minecraft/commands/Yedel.js
+++ src/minecraft/commands/Yedel.js
@@ -1,17 +1,26 @@
-const MinecraftCommand = require('../../contracts/MinecraftCommand')
+const MinecraftCommand = require('../../contracts/MinecraftCommand');
 
 class yedel extends MinecraftCommand {
   constructor(minecraft) {
-    super(minecraft)
+    super(minecraft);
 
-    this.name = 'yedel'
-    this.aliases = []
-    this.description = 'very cool command'
+    this.name = 'yedel';
+    this.aliases = [];
+    this.description = 'very cool command';
+    this.responses = [
+      'Yedel said {username} sucks',
+      'Hey {username}, Yedel thinks you suck!',
+      'Yedel is not a fan of {username}',
+    ];
   }
 
   onCommand(username, message) {
-    this.send(`/gc yedel said ${username} sucks`)
+    // Randomly select a response from the list
+    const randomIndex = Math.floor(Math.random() * this.responses.length);
+    const response = this.responses[randomIndex].replace('{username}', username);
+
+    this.send(`/gc ${response}`);
   }
 }
 
-module.exports = yedel
+module.exports = yedel;
