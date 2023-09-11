6453b60349fed60f7d7bc9f5b58438e9d11d2a81
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-10 19:35:57 -0700

    added random numbers to end

--- src/minecraft/commands/Yedel.js
+++ src/minecraft/commands/Yedel.js
@@ -22,37 +22,44 @@ class Yedel extends MinecraftCommand {
 
     const currentTimestamp = Date.now();
 
+
     if (currentTimestamp - this.cooldownTimestamp >= 60000) {
 
       this.cooldownTimestamp = currentTimestamp;
-      
 
-    const totalProbability = this.responses.reduce((sum, response) => sum + response.probability, 0);
-
-
-    const randomNumber = Math.floor(Math.random() * totalProbability) + 1;
+      const totalProbability = this.responses.reduce((sum, response) => sum + response.probability, 0);
 
 
-    let selectedResponse;
-    let cumulativeProbability = 0;
+      const randomNumber = Math.floor(Math.random() * totalProbability) + 1;
 
-    for (const response of this.responses) {
-      cumulativeProbability += response.probability;
+      let selectedResponse;
+      let cumulativeProbability = 0;
 
-      if (randomNumber <= cumulativeProbability) {
-        selectedResponse = response.text;
-        break;
+      for (const response of this.responses) {
+        cumulativeProbability += response.probability;
+
+        if (randomNumber <= cumulativeProbability) {
+          selectedResponse = response.text;
+          break;
+        }
       }
-    }
+
+      const extraNumbers = [];
+      for (let i = 0; i < 5; i++) {
+        extraNumbers.push(Math.floor(Math.random() * 100)); 
+      }
+
+      const extraNumbersString = extraNumbers.join(', ');
 
  
-    const formattedResponse = selectedResponse.replace('{username}', username);
+      const formattedResponse = selectedResponse.replace('{username}', username) + ` [${extraNumbersString}]`;
 
-    this.send(`/w ${username} ${formattedResponse}`);
-  } else {
-    this.send(`/gc Sorry, you can use this command again in 1 minute.`);
+      this.send(`/w ${username} ${formattedResponse}`);
+    } else {
+
+      this.send(`/w ${username} Sorry, you can use this command again in 1 minute.`);
+    }
   }
-}
 }
 
 module.exports = Yedel;
