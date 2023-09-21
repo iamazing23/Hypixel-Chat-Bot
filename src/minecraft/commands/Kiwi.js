e929a672ddd8ed7c88a4bf772e850059f6e9e834
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-20 20:14:27 -0700

    Create Kiwi.js

---
+++ src/minecraft/commands/Kiwi.js
@@ -0,0 +1,25 @@
+const MinecraftCommand = require('../../contracts/MinecraftCommand');
+
+class KiwiCommand extends MinecraftCommand {
+  constructor(minecraft) {
+    super(minecraft);
+
+    this.name = 'kiwi';
+    this.aliases = [k];
+    this.description = 'kiwis command';
+    this.responses = [
+      'Average kiwi moment ^^',
+      'Average kiwi moment ^^^^',
+      'Average kiwi moment ^',
+      'Average kiwi moment ^^^^^',
+    ];
+  }
+
+  onCommand(username, message) {
+    const randomIndex = Math.floor(Math.random() * this.responses.length);
+    const randomResponse = this.responses[randomIndex];
+    this.send(`/gc ${randomResponse}`);
+  }
+}
+
+module.exports = KiwiCommand;
