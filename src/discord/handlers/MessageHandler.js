c024ce43b7f72b0882fcc068529432ee659f6073
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-22 16:10:29 -0700

    a

--- src/discord/handlers/MessageHandler.js
+++ src/discord/handlers/MessageHandler.js
@@ -1,25 +1,21 @@
-const Discord = require('discord.js');
-
 class MessageHandler {
   constructor(discord, command) {
-    this.discord = discord;
-    this.command = command;
+    this.discord = discord
+    this.command = command
   }
 
   async onMessage(message) {
     if (!this.shouldBroadcastMessage(message)) {
-      return;
+      return
     }
 
     if (this.command.handle(message)) {
-      return;
+      return
     }
 
-    const content = this.stripDiscordContent(message.content).trim();
+    const content = this.stripDiscordContent(message.content).trim()
     if (content.length == 0) {
-
-      message.react('❌');
-      return;
+      return
     }
 
     if (this.isBlacklistedWord(message.content)) {
@@ -27,43 +23,38 @@ class MessageHandler {
         channel.send({
           embed: {
             author: { name: `Do not send that profanity!` },
-            color: 'FF0000',
-          },
-        });
-
-        message.react('❌');
-      });
-      return;
+            color: 'FF0000'
+          }
+        })
+      })
+      return
     }
 
     this.discord.broadcastMessage({
       username: message.member.displayName,
       message: this.stripDiscordContent(message.content),
       replyingTo: await this.fetchReply(message),
-    });
-
-    message.react('✅');
+    })
   }
 
   isBlacklistedWord(message) {
     const blacklistedWords = this.discord.app.config.discord.blacklistedWords;
     for (var i = 0; i < blacklistedWords.length; i++) { 
       if (message.includes(blacklistedWords[i])) {
-        return true;
-      }
-    }
-    return false; 
+        return true
+      };
+    };
   }
 
   async fetchReply(message) {
     try {
-      if (!message.reference) return null;
+      if (!message.reference) return null
 
-      const reference = await message.channel.messages.fetch(message.reference.messageID);
+      const reference = await message.channel.messages.fetch(message.reference.messageID)
 
-      return reference.member ? reference.member.displayName : reference.author.username;
+      return reference.member ? reference.member.displayName : reference.author.username
     } catch (e) {
-      return null;
+      return null
     }
   }
 
@@ -74,16 +65,16 @@ class MessageHandler {
       .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, '\n')
       .split('\n')
       .map(part => {
-        part = part.trim();
+        part = part.trim()
 
-        return part.length == 0 ? '' : part + ' ';
+        return part.length == 0 ? '' : part + ' '
       })
-      .join('');
+      .join('')
   }
 
   shouldBroadcastMessage(message) {
-    return !message.author.bot && message.channel.id == this.discord.app.config.discord.channel && message.content && message.content.length > 0;
+    return !message.author.bot && message.channel.id == this.discord.app.config.discord.channel && message.content && message.content.length > 0
   }
 }
 
-module.exports = MessageHandler;
+module.exports = MessageHandler
