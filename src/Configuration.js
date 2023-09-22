9825f0db7ee08aa4b119bb128fe0cfd165ebb45a
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-09-22 16:06:44 -0700

    aa

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,80 +1,92 @@
 const fs = require('fs')
 
 class Configuration {
-	properties = {
-		server: {
-			host: 'localhost',
-			port: 25565,
-		},
-		minecraft: {
-			username: null,
-			password: null,
-			lobbyHolder: null,
-			accountType: 'microsoft',
-		},
-		discord: {
-			token: null,
-			clientid:null,
-			guildid: null,
-			gcchannel: null,
-			dmchannel: null,
-			occhannel: null,
-			joinleavechannel: null,
-			commanderIds: null,
-			ownerId: null,
-			prefix: "+",
-			messageMode: "webhook",
-			thumbnail: "https://",
-			guildname: null
-			
-		},
-	}
+  properties = {
+    server: {
+      host: 'localhost',
+      port: 25565,
+    },
+    minecraft: {
+      username: null,
+      password: null,
+      lobbyHolder: null,
+      accountType: 'mojang',
+    },
+    discord: {
+      token: null,
+      channel: null,
+      commandRole: '',
+      ownerId: '',
+      prefix: '!',
+      messageMode: 'bot',
+      autoAccept: false,
+      weightRequirements: 0,
+      blacklistedWords: ["nigger", "negro", "cunt", "faggot", "bitch", "fuck", "gay", "fag", "shit", "slag", "slut", "hitler", "nazi", 'dick', 'ch1t', 
+                          'd1ck', 'dik', 'd1c', 'dichead', 'azz', 'c0ck', 'cock', 'penis', 'vagina', 'vegina', 'vejina', 'vajina', 'muzerfucer', 'motherfucker', 
+                          'kock', 'koc', 'k0ck', 'f0k', 'fock', 'kys', 'fuck', 'cunt', 'cvnt', 'dickhead', 'asshole', 'arsehole', 'fuckhead', 
+                          'fag', 'fags', 'faggot', 'faggots', 'feggit', 'feggits', 'faggit', 'faggits', 'cunts', 'kkk', 'whore', 'whores', 'slut', 'sluts', 
+                          'cunt', 'cunts', 'dickhead', 'dickheads', 'fucktard', 'fucker', 'fvcker', 'fuckerrs', 'fvckers', 'fckkers', 'fucking', 'fvcking', 
+                          'shit', 'shart', 'shite', 'whalecum', 'pussy', 'cock', 'dick', 'twat', 'hump', 'whitey', 'redneck', 'rednecks', 'chingchong', 'darky', 
+                          'darkey', 'darkie', 'darkies', 'darkys', 'darkeys', 'redskin', 'redskins', 'squinty', 'tacohead', 'anal', 'bastard', 'blowjob', 'boner', 
+                          'boob', 'boobs', 'boobies', 'boobeez', 'boobiez', 'boobz', 'tit', 'tits', 'titties', 'dildo', 'vagina', 'whore', 'cum', 'heil', 'sex', 
+                          'piss', 'bitch', 'fap', 'rape', 'raped', 'kekma'],
+      "blacklistedPlayers": []
+    },
+    express: {
+      enabled: false,
+      port: 8880,
+      authorization: "authorizationHeaderString"
+    }
+  }
+  
 
-	environmentOverrides = {
-		SERVER_HOST: val => (this.properties.server.host = val),
-		SERVER_PORT: val => (this.properties.server.port = val),
-		MINECRAFT_USERNAME: val => (this.properties.minecraft.username = val),
-		MINECRAFT_PASSWORD: val => (this.properties.minecraft.password = val),
-		MINECRAFT_LOBBY_HOLDER: val => (this.properties.minecraft.lobbyHolder = val),
-		MINECRAFT_ACCOUNT_TYPE: val => (this.properties.minecraft.accountType = val),
-		DISCORD_TOKEN: val => (this.properties.discord.token = val),
-		DISCORD_CLIENTID: val => (this.properties.discord.clientid = val),
-      	DISCORD_GUILDID: val => (this.properties.discord.guildid = val),
-      	DISCORD_GCCHANNEL: val => (this.properties.discord.gcchannel = val),
-      	DISCORD_DMCHANNEL: val => (this.properties.discord.dmchannel = val),
-      	DISCORD_OCCHANNEL: val => (this.properties.discord.occhannel = val),
-      	DISCORD_JOINLEAVECHANNEL: val => (this.properties.discord.joinleavechannel = val),
-      	DISCORD_COMMANDERIDS: val => (this.properties.discord.commanderIds = val),
-      	DISCORD_OWNERID: val => (this.properties.discord.ownerId = val),
-      	DISCORD_PREFIX: val => (this.properties.discord.prefix = val),
-      	DISCORD_MESSAGEMODE: val => (this.properties.discord.messageMode = val),
-      	DISCORD_THUMBNAIL: val => (this.properties.discord.thumbnail = val),
-      	DISCORD_GUILDNAME: val => (this.properties.discord.guildname = val)
-	}
+  environmentOverrides = {
+    SERVER_HOST: val => (this.properties.server.host = val),
+    SERVER_PORT: val => (this.properties.server.port = val),
+    MINECRAFT_USERNAME: val => (this.properties.minecraft.username = val),
+    MINECRAFT_PASSWORD: val => (this.properties.minecraft.password = val),
+    MINECRAFT_LOBBY_HOLDER: val => (this.properties.minecraft.lobbyHolder = val),
+    MINECRAFT_ACCOUNT_TYPE: val => (this.properties.minecraft.accountType = val),
+    DISCORD_TOKEN: val => (this.properties.discord.token = val),
+    DISCORD_CHANNEL: val => (this.properties.discord.channel = val),
+    DISCORD_COMMAND_ROLE: val => (this.properties.discord.commandRole = val),
+    DISCORD_OWNER_ID: val => (this.properties.discord.ownerId = val),
+    DISCORD_PREFIX: val => (this.properties.discord.prefix = val),
+    MESSAGE_MODE: val => (this.properties.discord.messageMode = val),
+    AUTO_ACCEPT: val => (this.properties.discord.autoAccept = val),
+    WEIGHT_REQUIREMENTS: val => (this.properties.discord.weightRequirements = val),
+    EXPRESS_ENABLED: val => (this.properties.express.enabled = val),
+    EXPRESS_PORT: val => (this.properties.express.enabled = val),
+    EXPRESS_AUTHORIZATION: val => (this.properties.express.authorization = val)
+  }
 
-	constructor() {
-		if (fs.existsSync('config.json')) {
-			this.properties = require('../config.json')
-		}
+  constructor() {
+    if (fs.existsSync('config.json')) {
+      this.properties = require('../config.json')
+    }
 
-		for (let environment of Object.keys(process.env)) {
-			if (this.environmentOverrides.hasOwnProperty(environment)) {
-				this.environmentOverrides[environment](process.env[environment])
-			}
-		}
-	}
+    for (let environment of Object.keys(process.env)) {
+      if (this.environmentOverrides.hasOwnProperty(environment)) {
+        this.environmentOverrides[environment](process.env[environment])
+      }
+    }
+  }
 
-	get server() {
-		return this.properties.server
-	}
+  get server() {
+    return this.properties.server
+  }
 
-	get minecraft() {
-		return this.properties.minecraft
-	}
+  get minecraft() {
+    return this.properties.minecraft
+  }
 
-	get discord() {
-		return this.properties.discord
-	}
+  get discord() {
+    return this.properties.discord
+  }
+
+  get express() {
+    return this.properties.express
+  }
 }
 
 module.exports = Configuration
