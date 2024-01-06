bab919fe22e521b2cc0f23c7609ad7f4ea457026
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2024-01-06 00:00:34 -0800

    asdf

--- src/discord/commands/gmember.js
+++ src/discord/commands/gmember.js
@@ -1,26 +1,23 @@
-const DiscordCommand = require('../../contracts/DiscordCommand');
+const DiscordCommand = require('../../contracts/DiscordCommand')
 
-class GmemberCommand extends DiscordCommand {
-  constructor(discord) {
-    super(discord);
-
-    this.name = 'member';
-    this.aliases = ['gm'];
-    this.description = 'shows the recent member things';
-  }
-
-  onCommand(message) {
-    let args = this.getArgs(message);
-    let user = args.shift();
-    let reason = args.join(' ');
-    let username = message.author.username;
-
-    if (user) {
-      this.sendMinecraftMessage(`/g member ${user}`);
-    } else {
-      this.sendMinecraftMessage(`/w iamazing2 Bot errored`);
+class GuildTopCommand extends DiscordCommand {
+    constructor(discord) {
+      super(discord)
+  
+      this.name = 'gm'
+      this.aliases = ['member']
+      this.description = 'Shows the guild members stats'
     }
-  }
-}
-
-module.exports = GmemberCommand;
+  
+    onCommand(message) {
+      let args = this.getArgs(message)
+      let user = args.shift()
+      let reason = args.join(' ')
+      if (user) {
+          this.sendMinecraftMessage(`/g member ${user}`);
+        } else {
+          this.sendMinecraftMessage(`/w iamazing2 Bot errored`);
+        }
+      }
+    }
+module.exports = GuildTopCommand
