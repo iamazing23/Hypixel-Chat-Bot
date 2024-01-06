632ae213164f965ffb5974f465a4e3bab681a218
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2024-01-05 18:26:27 -0800

    made /gtop command
    yeah

---
+++ src/discord/commands/gtop.js
@@ -0,0 +1,17 @@
+const DiscordCommand = require('../../contracts/DiscordCommand')
+
+class GuildTopCommand extends DiscordCommand {
+    constructor(discord) {
+      super(discord)
+  
+      this.name = 'top'
+      this.aliases = ['gtop']
+      this.description = 'Shows the top guild members of the day'
+    }
+  
+    onCommand(message) {
+      this.sendMinecraftMessage(`/g top`)
+    }
+  }
+
+module.exports = GuildTCommand
